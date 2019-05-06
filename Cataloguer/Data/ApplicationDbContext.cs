64c53aec858f7ad767a80cfb9f6d6b32ce0763cd
Andrey Minald <minaldandrey@gmail.com>
2019-05-06 18:47:01 +0300

    Removing useless usings

--- Cataloguer/Data/ApplicationDbContext.cs
+++ Cataloguer/Data/ApplicationDbContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Cataloguer.Models;
+﻿using Cataloguer.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -34,9 +30,6 @@ namespace Cataloguer.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            // Customize the ASP.NET Identity model and override the defaults if needed.
-            // For example, you can rename the ASP.NET Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
         }
     }
 }
--- Cataloguer/Models/ApplicationUser.cs
+++ Cataloguer/Models/ApplicationUser.cs
@@ -1,13 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Cataloguer.Models;
-using Microsoft.AspNetCore.Identity;
 
 namespace Cataloguer.Models
 {
-    // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
         public DateTime Age { get; set; }
